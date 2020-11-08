15f401a76d85cc93913315d5d8dfbc40037bc392
josef-m-bolt <josef.m.bolt@gmail.com>
2020-11-08 14:20:08 +0000

    First SDL stuff

--- src/tile_engine.cr
+++ src/tile_engine.cr
@@ -1,6 +1,30 @@
-# TODO: Write documentation for `TileEngine`
+require "../lib/sdl/src/sdl"
+
 module TileEngine
   VERSION = "0.1.0"
+  SDL.init(SDL::Init::VIDEO)
+  at_exit { SDL.quit }
 
-  # TODO: Put your code here
+  WINDOW_WIDTH = 640
+  WINDOW_HEIGHT = 480
+  TILE_WIDTH = 16
+  TILE_HEIGHT = 16
+  MAP_TILES_IN_ROW = WINDOW_WIDTH / TILE_WIDTH
+  MAP_TILES_IN_COLUMN = WINDOW_HEIGHT / TILE_HEIGHT
+
+  window = SDL::Window.new("Game!", WINDOW_WIDTH, WINDOW_HEIGHT)
+  renderer = SDL::Renderer.new(window)
+
+  loop do
+    event = SDL::Event.wait
+
+    case event
+    when SDL::Event::Quit
+      break
+    when SDL::Event::Keyboard
+      if event.mod.lctrl? && event.sym.q?
+        break
+      end
+    end
+  end
 end
