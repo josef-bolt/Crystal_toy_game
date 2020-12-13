ab0b6ca91e2edbcfc76eb1409522b282815b1ca5
josef-m-bolt <josef.m.bolt@gmail.com>
2020-12-13 13:48:02 +0000

    Implement updating game views

--- src/game_state_view.cr
+++ src/game_state_view.cr
@@ -2,16 +2,16 @@ require "./game_state.cr"
 require "./game.cr"
 
 class GameStateView
-  @view_bounds : NamedTuple(x_min: Int32, x_max: Int32, y_min: Int32, y_max: Int32)
+  @current_view_bounds : NamedTuple(x_min: Int32, x_max: Int32, y_min: Int32, y_max: Int32)
 
   def initialize(game : Game, game_state : GameState)
     @game = game
     @game_state = game_state
-    @view_bounds = init_view_bounds
+    @current_view_bounds = current_view_bounds
   end
 
   def update
-    update_view_bounds if !in_view?(focal_location)
+    @current_view_bounds = current_view_bounds if !in_view?(focal_location)
     render
   end
 
@@ -34,34 +34,41 @@ class GameStateView
     end
   end
 
+  private def current_view_bounds
+    x_max = view_x_max
+    y_max = view_y_max
+    {
+      x_min: x_max - Game::MAP_TILE_WIDTH,
+      x_max: x_max,
+      y_min: y_max - Game::MAP_TILE_HEIGHT,
+      y_max: y_max
+    }
+  end
+
+  private def view_x_max
+    pos = focal_location[0]
+    (1..3).map { |i| i*Game::MAP_TILE_WIDTH }.select { |b| b > pos }.first
+  end
+
+  private def view_y_max
+    pos = focal_location[1]
+    (1..3).map { |i| i*Game::MAP_TILE_HEIGHT }.select { |b| b > pos }.first
+  end
+
   private def in_view?(location)
-    location[0] >= @view_bounds[:x_min] &&
-      location[0] < @view_bounds[:x_max] &&
-      location[1] >= @view_bounds[:y_min] &&
-      location[1] < @view_bounds[:y_max]
+    location[0] >= @current_view_bounds[:x_min] &&
+      location[0] < @current_view_bounds[:x_max] &&
+      location[1] >= @current_view_bounds[:y_min] &&
+      location[1] < @current_view_bounds[:y_max]
   end
 
   private def screen_pos(entity)
     {
-      (entity.location[0] - (@view_bounds[:x_min]) + Game::LEFT_PANEL_TILE_WIDTH) * Game::TILE_WIDTH,
-      (entity.location[1] - @view_bounds[:y_min]) * Game::TILE_HEIGHT,
+      (entity.location[0] - (@current_view_bounds[:x_min]) + Game::LEFT_PANEL_TILE_WIDTH) * Game::TILE_WIDTH,
+      (entity.location[1] - @current_view_bounds[:y_min]) * Game::TILE_HEIGHT,
       Game::TILE_WIDTH,
       Game::TILE_HEIGHT
     }
-  end
-
-  private def init_view_bounds
-    x_min = Math.max(0, focal_location[0] - (Game::WINDOW_TILE_WIDTH // 2))
-    y_min = Math.max(0, focal_location[1] - (Game::WINDOW_TILE_HEIGHT // 2))
-    {
-      x_min: x_min,
-      x_max: Math.min(@game_state.map.size, x_min + Game::WINDOW_TILE_WIDTH),
-      y_min: y_min,
-      y_max: Math.min(@game_state.map.first.size, y_min + Game::WINDOW_TILE_HEIGHT)
-    }
-  end
-
-  private def update_view_bounds
   end
 
   private def render_interface
